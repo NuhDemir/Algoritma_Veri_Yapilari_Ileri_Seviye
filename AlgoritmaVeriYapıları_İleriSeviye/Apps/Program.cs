58d00908f03547a0ee0333daae3c7c40ad33d5fe
NuhDemir <drmaa217@gmail.com>
2024-06-25 16:42:22 +0300

    new

---
+++ AlgoritmaVeriYapıları_İleriSeviye/Apps/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+ 
